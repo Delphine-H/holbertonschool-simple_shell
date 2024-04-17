a4ced0cf5b62cd6fa2e0613350798285552ce6d3
Delphine-H <hannon.delphine@gmail.com>
2024-04-17 15:47:41 +0200

    Initial commit: Implementation of memory deallocation function
    Co-authored-by: antrinarich <antrinarich@gmail.com>

---
+++ free.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_tokens - function that free an array of tokens
+ * @array_tokens: array to free
+*/
+
+void free_tokens(char **array_tokens)
+{
+    int index = 0;
+
+    while (array_tokens[index] != NULL)
+    {
+        free(array_tokens[index]);
+        index++;
+    }
+    free(array_tokens);
+}
