0fcbb5197c8e8b9848288bc66ee8f668ec61d665
Delphine-H <hannon.delphine@gmail.com>
2024-04-17 15:41:51 +0200

    Initial commit: Implementation of utility functions for tokenization
    Co-authored-by: Antrinarich <antrinarich@gmail.com>

---
+++ get_token.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _get_token - function that splits a string and returns an array of each
+ * word of the string.
+ * @buffer: string to split
+ * Return: an array of each word
+*/
+
+char **_get_token(char *buffer)
+{
+    int max_tokens = 15;
+    char *token;
+    char **array_tokens = NULL;
+    int argCount = 0;
+
+    array_tokens = malloc(max_tokens * sizeof(char *));
+
+    if (array_tokens == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(buffer, " \t\n");
+    while (token != NULL && argCount < max_tokens - 1) 
+    {
+        array_tokens[argCount] = strdup(token);
+        if (array_tokens[argCount] == NULL)
+        {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+    
+        argCount++;
+
+        token = strtok(NULL, " \t\n"); /* get the next token */
+    }
+    array_tokens[argCount] = NULL;
+
+    return array_tokens;
+}
