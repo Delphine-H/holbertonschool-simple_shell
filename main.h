b0f8b24024b3252b703864ae8d85a3552c952979
LouisXV16 <louis.saintmachin@gmail.com>
2024-04-19 16:47:27 +0200

    Update main.h with included headers

--- main.h
+++ main.h
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
 
 char **_get_token(char *buffer);
 void free_tokens(char **array_tokens);
